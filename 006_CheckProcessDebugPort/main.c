2ed1115449d9eb526ad2d262cb94a4eddda3d129
Marcos Alvares <marcos.alvares@fireeye.com>
2020-12-02 20:34:46 +0100

    Adding check for ProcessDebugPort.

---
+++ 006_CheckProcessDebugPort/main.c
@@ -0,0 +1,25 @@
+// @marcos_alvares
+// build with "cl.exe main.c /link ntdll.dll"
+
+#include <windows.h>
+#include <stdlib.h>
+#include <winternl.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    NTSTATUS status;
+    DWORD debug_port = 0;
+
+    status = NtQueryInformationProcess(
+        GetCurrentProcess(),
+        ProcessDebugPort,
+        &debug_port,
+        sizeof(debug_port),
+        NULL);
+
+    if (status == 0x00000000 && debug_port != 0) {
+        printf("[+] Sandbox has been detected.\n");
+        return 0;
+    }
+    return 0;
+}
